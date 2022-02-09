8e1643762a8477b387a96b0bfdf094f37bdf2d39
Jose <josejesuscedeno@gmail.com>
2022-02-09 19:13:59 +0000

    creados divs

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,18 @@ import "../styles/index.css";
 
 //import your own components
 function SimpleCounter(props) {
-	return <div className="bigCounter">
-        <div className="calendar">
-            <i class="fa-regular fa-clock"></i>
-            </div>
-        <div className="seconds"></div>
-        <div className="four"></div>
-        <div className="three"></div>
-        <div className="two"></div>
-        <div className="one"></div>
-    </div>;
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i class="far fa-clock"></i>
+			</div>
+			<div className="seconds">0</div>
+			<div className="four">0</div>
+			<div className="three">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+		</div>
+	);
 }
 //render your react application
 ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
