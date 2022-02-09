37dda152c2c211929d3c040f4704e0a31d5f75ca
Jose <josejesuscedeno@gmail.com>
2022-02-09 20:17:53 +0000

    dd

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 //create your first component
 const Home = (props) => {
@@ -8,15 +8,18 @@ const Home = (props) => {
 			<div className="calendar">
 				<i className="far fa-clock"></i>
 			</div>
-			<div className="four">4</div>
-			<div className="three">3</div>
-			<div className="two">0</div>
+			<div className="four">{props.four}</div>
+			<div className="three">{props.three}</div>
+			<div className="two">{props.two}</div>
 			<div className="one">{props.one}</div>
 		</div>
 	);
 };
 
 Home.propTypes = {
-	one: PropTypes.number
-  };
+	one: PropTypes.number,
+	two: PropTypes.number,
+	three: PropTypes.number,
+	four: PropTypes.number,
+};
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,17 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //import your own components
+let counter = 0;
+setInterval(function () {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	counter++;
+}, 1000);
 
-//render your react application
-ReactDOM.render(<Home one={1}/>, document.querySelector("#app"));
+ReactDOM.render(
+	<Home one={one} two={two} three={three} four={four} />,
+	document.querySelector("#app")
+);
