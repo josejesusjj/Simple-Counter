14aff7f8f273144d451ca96055ad404aba95e2b4
Jose <josejesuscedeno@gmail.com>
2022-02-09 19:59:02 +0000

    one

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from 'prop-types';
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="four">4</div>
+			<div className="three">3</div>
+			<div className="two">0</div>
+			<div className="one">{props.one}</div>
 		</div>
 	);
 };
 
+Home.propTypes = {
+	one: PropTypes.number
+  };
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -4,21 +4,9 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import Home from "./component/home.jsx";
 
 //import your own components
-function SimpleCounter(props) {
-	return (
-		<div className="bigCounter">
-			<div className="calendar">
-				<i class="far fa-clock"></i>
-			</div>
-			<div className="seconds">0</div>
-			<div className="four">0</div>
-			<div className="three">0</div>
-			<div className="two">0</div>
-			<div className="one">0</div>
-		</div>
-	);
-}
+
 //render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+ReactDOM.render(<Home one={1}/>, document.querySelector("#app"));
