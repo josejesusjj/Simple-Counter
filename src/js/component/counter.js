21b431c581929bc535caa93204878bc687cd181d
Jose <josejesuscedeno@gmail.com>
2022-02-23 11:03:52 +0000

    vamos bien

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -2,15 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Counter = (props) => {
+	let seconds = 0;
+	setInterval(function () {
+		const miles = Math.floor(seconds / 1000);
+		const hundreds = Math.floor(seconds / 100);
+		const tens = Math.floor(seconds / 10);
+		const units = Math.floor(seconds);
+		seconds++;
+		console.log(miles, hundreds, tens, units);
+		return seconds;
+	}, 1000);
+
 	return (
 		<div className="bigCounter">
 			<div className="calendar">
 				<i className="far fa-clock"></i>
 			</div>
-			<div className="four">{props.miles}</div>
-			<div className="three">{props.hundreds}</div>
-			<div className="two">{props.tens}</div>
-			<div className="one">{props.units}</div>
+			<div>{props.miles}</div>
+			<div>{props.hundreds}</div>
+			<div>{props.tens}</div>
+			<div>{props.units}</div>
 		</div>
 	);
 };
@@ -21,16 +32,5 @@ Counter.propTypes = {
 	hundreds: PropTypes.number,
 	miles: PropTypes.number,
 };
-
-let counter = 0;
-setInterval(function () {
-	const miles = Math.floor(counter / 1000);
-	const hundreds = Math.floor(counter / 100);
-	const tens = Math.floor(counter / 10);
-	const units = Math.floor(counter);
-	counter++;
-	console.log(miles, hundreds, tens, units);
-	return counter;
-}, 1000);
 
 export default Counter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,7 +4,8 @@ import Counter from "./counter";
 const Home = () => {
 	return (
 		<div>
-			<Counter units={0} />
+			<Counter miles={0} hundreds={0} tens={0} units={0} />
+
 			<h1 className="text-center mt-5">Just a Simple Counter!</h1>
 			<h2 className="text-center mt-5">Made by josejesusjj</h2>
 		</div>
